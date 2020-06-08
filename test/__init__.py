b28d199ad5f229e131065f8551ec0323be9c5562
Tinh Nguyen <tinhtn1508@gmail.com>
2020-06-08 22:42:33 +0700

    Remove unused code

--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,1 @@
-import sys
-sys.path.insert(0, './..')
-import graph
-
-from .adjacency_matrix_test import AdjacencyMatrixTest, AdjacencyMatrixTestSuite
+from .adjacency_matrix_test import AdjacencyMatrixTestSuite

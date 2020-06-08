c73b6a99845d2d3a0bfed87ab70e2568e10b2304
Tinh Nguyen <tinhtn1508@gmail.com>
2020-06-09 01:11:09 +0700

    Update testsuites for stack and queue

--- test.py
+++ test.py
@@ -2,9 +2,16 @@ import test
 import unittest
 
 def main():
-    print("Running unit test for Adjacency matrix class")
     runner = unittest.TextTestRunner()
+
+    print("Running unit test for Adjacency matrix class")
     runner.run(test.AdjacencyMatrixTestSuite())
+
+    print("Running unit test for Stack class")
+    runner.run(test.StackTestSuite())
+
+    print("Running unit test for Queue class")
+    runner.run(test.QueueTestSuite())
 
 if __name__ == "__main__":
     main()

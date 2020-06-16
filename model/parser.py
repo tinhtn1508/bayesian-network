f86287f64afb9874b34d711363c4c22785212a72
Tinh Nguyen <tinhtn1508@gmail.com>
2020-06-16 11:20:09 +0700

    Add txt parser

--- model/parser.py
+++ model/parser.py
@@ -1,4 +1,4 @@
-import json
+import os
 from typing import (
     Dict,
     Optional,
@@ -14,12 +14,33 @@ from typing import (
 )
 
 
-class Parser:
-    def __init__(self):
-        pass
+class TxtParser:
+    def __init__(self, filePath: str):
+        self.__filePath = filePath
 
-    def fromJson(self):
-        raise NotImplementedError
+    def readLine(self) -> Generator[str, None, None]:
+        with open(self.__filePath, "r") as fp:
+            for line in fp:
+                yield line.strip("\n")
 
-    def fromTxt(self):
-        pass
+    def readLines(self) -> List:
+        return list(self.readLine())
+
+    def writeLine(self, buffer: str):
+        with open(self.__filePath, "a") as fp:
+            fp.writelines(buffer + "\n")
+
+    def writeLines(self, buffers: List[str]):
+        if os.path.exists(self.__filePath):
+            print(
+                "WARNING: {} is exists and the method will replace this file".format(
+                    self.__filePath
+                )
+            )
+        with open(self.__filePath, "w") as fp:
+            for buff in buffers:
+                fp.writelines(buff + "\n")
+
+    def __str__(self):
+        info = os.stat(self.__filePath)
+        return "{}".format(info)

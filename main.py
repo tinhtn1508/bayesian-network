31535d3241843e597aeb7b81d68e059fb8c107fa
Tinh Nguyen <tinhtn1508@gmail.com>
2020-06-28 01:58:05 +0700

    Use random of python and add required for argument

--- main.py
+++ main.py
@@ -86,8 +86,8 @@ class BayesianNetworkRunner:
 # @timeExecute
 def main() -> None:
     parser: ArgumentParser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", help="path file to model")
-    parser.add_argument("-t", "--test", help="path file to queries")
+    parser.add_argument("-m", "--model", required=True, help="path file to model")
+    parser.add_argument("-t", "--test", required=True, help="path file to queries")
     parser.add_argument(
         "-o", "--output", default="output.txt", help="path file to output"
     )
@@ -95,11 +95,6 @@ def main() -> None:
         "-a", "--algorithm", default="forward", help="forward | likelihood | gibbs"
     )
     args = parser.parse_args()
-    if args.model is None or args.test is None or args.algorithm is None:
-        warning = "\nThe agruments [--model, --test, --algorithm] can not be empty\n"
-        warning += "For more details, you can use the command [main.py -h]\n"
-        print(warning)
-        exit()
 
     try:
         BayesianNetworkRunner(args.model, args.test, args.algorithm, args.output).run()

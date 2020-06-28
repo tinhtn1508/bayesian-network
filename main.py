68b23a676425f6ac202b5590476e86e98a2a7bc2
Tinh Nguyen <tinhtn1508@gmail.com>
2020-06-28 09:18:33 +0700

    Add required for arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import graph
 import argparse
 import os
+from common import timeExecute
 from model import (
     Node,
     ModelParser,
@@ -64,23 +65,28 @@ class BayesianNetworkRunner:
         result: List[float] = self.__network.batchQuery(self.__queries)
         self.__output.writeLines([s for s in map(str, result)])
 
+
+@timeExecute
 def main() -> None:
     parser: ArgumentParser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", help = "path file to model")
-    parser.add_argument("-t", "--test", help = "path file to queries")
-    parser.add_argument("-o", "--output", default = "output.txt", help = "path file to output")
+    parser.add_argument("-m", "--model", required=True, help="path file to model")
+    parser.add_argument("-t", "--test", required=True, help="path file to queries")
     parser.add_argument(
-        "-a", "--algorithm", default = "forward", help = "forward | likelihood | gibbs"
+        "-o", "--output", default="output.txt", help="path file to output"
+    )
+    parser.add_argument(
+        "-a", "--algorithm", default="forward", help="forward | likelihood | gibbs"
     )
     args = parser.parse_args()
-    if args.model is None or args.test is None or args.algorithm is None:
-        warning = "\nThe agruments [--model, --test, --algorithm] can not be empty\n"
-        warning += "For more details, you can use the command [main.py -h]\n"
-        print(warning)
-        exit()
-    BayesianNetworkRunner(args.model, args.test, args.algorithm, args.output).run()
+
+    try:
+        BayesianNetworkRunner(args.model, args.test, args.algorithm, args.output).run()
+    except Exception as e:
+        print("Failed: {}".format(e))
+    else:
+        print("The inference procedure is successful !!!!!!!")
+        print("Please check output at here: {}".format(os.path.abspath(args.output)))
+
 
 if __name__ == "__main__":
     main()
-
-
